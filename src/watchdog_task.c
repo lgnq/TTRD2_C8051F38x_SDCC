90c0ee3be0f6f1e33b642ec4fc935b0e96b74b0b
lgnq <dzzxzz@gmail.com>
2017-06-02 10:17:51 +0800

    update watchdog init and update function

--- src/watchdog_task.c
+++ src/watchdog_task.c
@@ -3,7 +3,7 @@
   ttrd2-02a-t0401a-v001a_iwdt_task.c (Release 2017-02-22a)
 
   ----------------------------------------------------------------------------
-   
+
   'Watchdog' timer library (internal) for STM32F401RC.
 
 -*----------------------------------------------------------------------------*/
@@ -11,30 +11,30 @@
 
   This code is copyright (c) 2014-2016 SafeTTy Systems Ltd.
 
-  This code forms part of a Time-Triggered Reference Design (TTRD) that is 
-  documented in the following book: 
+  This code forms part of a Time-Triggered Reference Design (TTRD) that is
+  documented in the following book:
 
-   Pont, M.J. (2016) 
-   "The Engineering of Reliable Embedded Systems (Second Edition)", 
+   Pont, M.J. (2016)
+   "The Engineering of Reliable Embedded Systems (Second Edition)",
    Published by SafeTTy Systems Ltd. ISBN: 978-0-9930355-3-1.
 
-  Both the TTRDs and the above book ("ERES2") describe patented 
+  Both the TTRDs and the above book ("ERES2") describe patented
   technology and are subject to copyright and other restrictions.
 
-  This code may be used without charge: [i] by universities and colleges on 
-  courses for which a degree up to and including 'MSc' level (or equivalent) 
-  is awarded; [ii] for non-commercial projects carried out by individuals 
+  This code may be used without charge: [i] by universities and colleges on
+  courses for which a degree up to and including 'MSc' level (or equivalent)
+  is awarded; [ii] for non-commercial projects carried out by individuals
   and hobbyists.
 
-  Any and all other use of this code and / or the patented technology 
+  Any and all other use of this code and / or the patented technology
   described in ERES2 requires purchase of a ReliabiliTTy Technology Licence:
   http://www.safetty.net/technology/reliabilitty-technology-licences
 
-  Please contact SafeTTy Systems Ltd if you require clarification of these 
+  Please contact SafeTTy Systems Ltd if you require clarification of these
   licensing arrangements: http://www.safetty.net/contact
 
-  CorrelaTTor, DecomposiTTor, DuplicaTTor, MoniTTor, PredicTTor, ReliabiliTTy,  
-  SafeTTy, SafeTTy Systems, TriplicaTTor and WarranTTor are registered 
+  CorrelaTTor, DecomposiTTor, DuplicaTTor, MoniTTor, PredicTTor, ReliabiliTTy,
+  SafeTTy, SafeTTy Systems, TriplicaTTor and WarranTTor are registered
   trademarks or trademarks of SafeTTy Systems Ltd in the UK & other countries.
 
 -*----------------------------------------------------------------------------*/
@@ -46,20 +46,20 @@
   WATCHDOG_Init()
 
   Set up independent watchdog timer.
-   
+
   The watchdog timer is driven by the Internal LSI (RC) Oscillator:
   the timing varies (significantly) with temperature.
 
-  Datasheet values: 
-  RC oscillator frequency (Minimum) : 17 kHz  
-  RC oscillator frequency (Typical) : 32 kHz 
-  RC oscillator frequency (Maximum) : 47 kHz 
+  Datasheet values:
+  RC oscillator frequency (Minimum) : 17 kHz
+  RC oscillator frequency (Typical) : 32 kHz
+  RC oscillator frequency (Maximum) : 47 kHz
 
   It is suggested that you use twice the required timeout value (approx).
-    
+
   PARAMETERS:
-     WDT_COUNT : Will be multiplied by ~100¦Ìs to determine the timeout. 
-   
+     WDT_COUNT : Will be multiplied by ~100¦Ìs to determine the timeout.
+
   LONG-TERM DATA:
      None.
 
@@ -81,26 +81,42 @@
 -*----------------------------------------------------------------------------*/
 void WATCHDOG_Init(const uint32_t WDT_COUNT)
 {
-#if 0  
-   // Enable write access to IWDG_PR and IWDG_RLR registers
-   IWDG->KR = 0x5555;
+    // Even though the LFOSC is disabled after a reset and the register reads
+    // disabled, the oscillator is automatically enabled whenever it's used by
+    // the WDT. This means we do not need to enable it here.
 
-   // Set pre-scalar to 4 (timer resolution is 125¦Ìs)
-   IWDG->PR = 0x00;
+    // WDT interval given by:
+    //
+    // 4 ^ (3 + WDTCN[2:0]) * TLFOSC
+    //
+    // The LFOSC is running at 80 kHz
+    //
+    // We want a timeout of approximately 10 ms.
+    //
+    // 4 ^ (3 + 2) * 12.5 us = 12.8 ms
 
-   // Counts down to 0 in increments of 125¦Ìs
-   // Max reload value is 0xFFF (4095) or ~511 ms (with this prescalar)
-   IWDG->RLR = WDT_COUNT;
+    // WDTCN.7 must be cleared to 0 to write the timeout interval
+    ///WDTCN = 0x02;
 
-   // Reload IWDG counter
-   IWDG->KR = 0xAAAA;
+    // Enable and feed the watchdog
+    ///WDTCN = 0xA5;
 
-   // Enable IWDG (the LSI oscillator will be enabled by hardware)
-   IWDG->KR = 0xCCCC;
-#endif
-   
-   // Feed watchdog
-   WATCHDOG_Update();
+    /*
+    To configure the WDT, perform the following tasks:
+    1. Disable the WDT by writing a 0 to the WDTE bit.
+    2. Select the desired PCA clock source (with the CPS2–CPS0 bits). default is SYSCLK/12
+    3. Load PCA0CPL4 with the desired WDT update offset value.
+    4. Configure the PCA Idle mode (set CIDL if the WDT should be suspended while the CPU is in Idle mode).
+    5. Enable the WDT by setting the WDTE bit to 1.
+    6. Reset the WDT timer by writing to PCA0CPH4.
+    */
+
+    PCA0CPL4 = 0xFF;
+
+    PCA0MD |= 0x40;						// Enable Watchdog timer, WDTE = 1
+
+    // Feed watchdog
+    WATCHDOG_Update();
 }
 
 /*----------------------------------------------------------------------------*-
@@ -110,13 +126,13 @@ void WATCHDOG_Init(const uint32_t WDT_COUNT)
   Feed the watchdog timer.
 
   See Watchdog_Init() for further information.
-   
+
   PARAMETERS:
      None.
 
   LONG-TERM DATA:
      None.
-   
+
   MCU HARDWARE:
      iWDT hardware.
 
@@ -141,25 +157,20 @@ void WATCHDOG_Init(const uint32_t WDT_COUNT)
 -*----------------------------------------------------------------------------*/
 void WATCHDOG_Update(void)
 {
-   // Feed the watchdog (reload IWDG counter)
+    // Feed the watchdog (reload IWDG counter)
+    PCA0CPH4 = 0x0;
 }
 
 uint8_t reset_by_watchdog(void)
 {
-    /*
-    if (reset_by_watchdog)
+    if (RSTSRC == 0x08)
     {
-        clear the flag
-
         return 1;
     }
     else
     {
         return 0;
     }
-    */
-  
-    return 0;  
 }
 
 /*----------------------------------------------------------------------------*-
