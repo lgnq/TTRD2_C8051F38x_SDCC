9cb7f238bddc186af707cf03f11b4d8df6e08726
lgnq <dzzxzz@gmail.com>
2017-06-13 10:57:30 +0800

    update key task
    MODE key worked now

--- src/key_task.c
+++ src/key_task.c
@@ -3,7 +3,7 @@
   ttrd2-02a-t0401a-v001a_switch_task.c (Release 2017-02-22a)
 
   ----------------------------------------------------------------------------
-   
+
   Simple switch interface library for Nucleo F401 board.
 
 -*----------------------------------------------------------------------------*/
@@ -11,30 +11,30 @@
 
   This code is copyright (c) 2014-2016 SafeTTy Systems Ltd.
 
-  This code forms part of a Time-Triggered Reference Design (TTRD) that is 
-  documented in the following book: 
+  This code forms part of a Time-Triggered Reference Design (TTRD) that is
+  documented in the following book:
 
-   Pont, M.J. (2016) 
-   "The Engineering of Reliable Embedded Systems (Second Edition)", 
+   Pont, M.J. (2016)
+   "The Engineering of Reliable Embedded Systems (Second Edition)",
    Published by SafeTTy Systems Ltd. ISBN: 978-0-9930355-3-1.
 
-  Both the TTRDs and the above book ("ERES2") describe patented 
+  Both the TTRDs and the above book ("ERES2") describe patented
   technology and are subject to copyright and other restrictions.
 
-  This code may be used without charge: [i] by universities and colleges on 
-  courses for which a degree up to and including 'MSc' level (or equivalent) 
-  is awarded; [ii] for non-commercial projects carried out by individuals 
+  This code may be used without charge: [i] by universities and colleges on
+  courses for which a degree up to and including 'MSc' level (or equivalent)
+  is awarded; [ii] for non-commercial projects carried out by individuals
   and hobbyists.
 
-  Any and all other use of this code and / or the patented technology 
+  Any and all other use of this code and / or the patented technology
   described in ERES2 requires purchase of a ReliabiliTTy Technology Licence:
   http://www.safetty.net/technology/reliabilitty-technology-licences
 
-  Please contact SafeTTy Systems Ltd if you require clarification of these 
+  Please contact SafeTTy Systems Ltd if you require clarification of these
   licensing arrangements: http://www.safetty.net/contact
 
-  CorrelaTTor, DecomposiTTor, DuplicaTTor, MoniTTor, PredicTTor, ReliabiliTTy,  
-  SafeTTy, SafeTTy Systems, TriplicaTTor and WarranTTor are registered 
+  CorrelaTTor, DecomposiTTor, DuplicaTTor, MoniTTor, PredicTTor, ReliabiliTTy,
+  SafeTTy, SafeTTy Systems, TriplicaTTor and WarranTTor are registered
   trademarks or trademarks of SafeTTy Systems Ltd in the UK & other countries.
 
 -*----------------------------------------------------------------------------*/
@@ -68,7 +68,7 @@ static uint32_t Switch_button1_pressed_g = BUTTON1_NOT_PRESSED;
 
   LONG-TERM DATA:
      None.
-   
+
   MCU HARDWARE:
      GPIO pin (Switch pin)
 
@@ -88,8 +88,10 @@ static uint32_t Switch_button1_pressed_g = BUTTON1_NOT_PRESSED;
 void SWITCH_BUTTON1_Init(void)
 {
     //todo : init the pins of key
+    P3MDOUT = 0x00;
+    P3SKIP = 0xFF;
 
-    // Set the initial state  
+    // Set the initial state
     Switch_button1_pressed_g = BUTTON1_NOT_PRESSED;
 }
 
@@ -97,14 +99,14 @@ void SWITCH_BUTTON1_Init(void)
 
   SWITCH_BUTTON1_Update()
 
-  Switch-reading task. 
-   
+  Switch-reading task.
+
   PARAMETERS:
      None.
 
   LONG-TERM DATA:
      Duration_s (W)
-   
+
   MCU HARDWARE:
      GPIO pin (Switch pin)
 
@@ -133,10 +135,10 @@ void SWITCH_BUTTON1_Update(void)
     static uint32_t Duration_s = 0;
 
     uint32_t Button1_input = 1;
-        
+
     // Read the pin state
-    ///Button1_input = GPIO_ReadInputDataBit(BUTTON1_PORT, BUTTON1_PIN);
-   
+    Button1_input = MODE_KEY;
+
     if (Button1_input == SW_PRESSED)
     {
         Duration_s += 1;
@@ -168,13 +170,13 @@ void SWITCH_BUTTON1_Update(void)
   SWITCH_BUTTON1_Get_State()
 
   Reurns switch state.
-   
+
   PARAMETERS:
      None.
 
   LONG-TERM DATA:
      None.
-   
+
   MCU HARDWARE:
      None.
 
